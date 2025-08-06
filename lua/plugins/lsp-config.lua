58aefeabdd5845caa156befb7432c0fa979074ac
Nathan Dygant <nathan.dygant@gmail.com>
2025-08-06 22:45:02 +0200

    Adding elixir properly

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,6 +16,7 @@ return {
           "ts_ls",
           "pyright",
           "vuels",
+          "elixirls",
         },
       })
     end,
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,8 +4,9 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = { "lua", "javascript", "python", "typescript", "elixir" },
+			ensure_installed = { "lua", "javascript", "python", "typescript", "elixir", "eex", "heex" },
 			highlight = { enable = true },
+      auto_install = true,
 			indent = { enable = true },
 		})
 	end,
